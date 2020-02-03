0f514a59a878932c4a8d385dccabb3de7f30ea21
Atharva Bhardwaj <51410612+AtharvaB12@users.noreply.github.com>
2020-02-03 16:00:43 +0530

    Bash script file to Launch an aws instance with security group

---
+++ my_script.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+#SEC_ID12=`aws ec2 create-security-group --group-name sec_group_01 --description "Default Security Group" --vpc-id vpc-6664411c --output text`
+#echo $SEC_ID12
+aws ec2 authorize-security-group-ingress --group-name sec_group_01 --protocol tcp --port 80 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-name sec_group_01 --protocol tcp --port 443 --cidr 0.0.0.0/0
+
+#New=`aws ec2 run-instances --image-id ami-09d069a04349dc3cb --count 1 --instance-type t2.micro --key-name aws_key --security-group-ids=$SEC_ID12  --placement AvailabilityZone=us-east-1a  --block-device-mappings DeviceName=/dev/xvda,Ebs={VolumeSize=10}`
